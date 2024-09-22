fe9c63cb31ae2f5134271dcffe33874bea150de8
khalilarfaoui <khalilwin1920@gmail.com>
2024-09-22 11:46:40 +0100

    sonarcloud-prop2

--- src/main/java/com/application/piplinecicd/model/Employee.java
+++ src/main/java/com/application/piplinecicd/model/Employee.java
@@ -1,12 +1,11 @@
 package com.application.piplinecicd.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
